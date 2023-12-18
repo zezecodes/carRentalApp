9cba84388b3bd7da89f1cf9242c8e032ce1b3c0b
Aaron Djangmah <djangsiness01@gmail.com>
2023-12-18 20:46:45 +0000

    updates

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -14,139 +14,271 @@ class _HomeState extends State<Home> {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
+        resizeToAvoidBottomInset: true,
         backgroundColor: Colors.grey[700],
         body: SafeArea(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: CircleAvatar(
-                      radius: 40,
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: CircleAvatar(
+                        radius: 40,
+                      ),
                     ),
-                  ),
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hello, Aaron',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.yellow,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        'Welcome Back',
-                        style: TextStyle(fontSize: 15, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 140),
-                    child: IconButton.outlined(
-                        iconSize: 30,
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hello, Aaron',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Welcome Back',
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 140),
+                      child: IconButton.outlined(
+                          iconSize: 30,
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.notifications,
+                            color: Colors.white,
+                          )),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  child: SearchBar(
+                    elevation: MaterialStateProperty.all(3),
+                    backgroundColor: MaterialStatePropertyAll(Colors.grey[800]),
+                    hintText: 'Search',
+                    hintStyle: MaterialStateProperty.all(
+                        const TextStyle(color: Colors.white)),
+                    leading: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    trailing: [
+                      IconButton(
                         onPressed: () {},
                         icon: const Icon(
-                          Icons.notifications,
+                          Icons.filter_list_outlined,
                           color: Colors.white,
-                        )),
-                  )
-                ],
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                child: SearchBar(
-                  elevation: MaterialStateProperty.all(3),
-                  backgroundColor: MaterialStatePropertyAll(Colors.grey[800]),
-                  hintText: 'Search',
-                  hintStyle: MaterialStateProperty.all(
-                      const TextStyle(color: Colors.white)),
-                  leading: const Icon(
-                    Icons.search,
-                    color: Colors.white,
+                        ),
+                      )
+                    ],
                   ),
-                  trailing: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.filter_list_outlined,
-                        color: Colors.white,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[600],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            CupertinoIcons.car_detailed,
+                            color: Colors.yellow,
+                          ),
+                          Text(
+                            'Buy Car',
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 130,
+                      height: 120,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[600],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(CupertinoIcons.car_detailed),
+                          Text('Buy Car')
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 130,
+                      height: 120,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[600],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(CupertinoIcons.car_detailed),
+                          Text('Buy Car')
+                        ],
                       ),
                     )
                   ],
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 130,
-                    height: 120,
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Container(
+                    width: 410,
+                    height: 170,
                     decoration: BoxDecoration(
-                      color: Colors.grey[600],
+                      color: Colors.grey[800],
                       borderRadius: BorderRadius.circular(20),
                     ),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    child: Column(
                       children: [
-                        Icon(
-                          CupertinoIcons.car_detailed,
-                          color: Colors.yellow,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                'Search by Brands',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStatePropertyAll(
+                                        Colors.grey[800]),
+                                    side: const MaterialStatePropertyAll(
+                                        BorderSide(color: Colors.yellow))),
+                                onPressed: () {},
+                                child: const Text(
+                                  'See all',
+                                  style: TextStyle(color: Colors.yellow),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                        Text(
-                          'Buy Car',
-                          style: TextStyle(color: Colors.white),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          physics: const BouncingScrollPhysics(),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 85,
+                                height: 85,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[900],
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Center(
+                                  child: Image.asset(
+                                    'assets/images/bmw.png',
+                                    width: 50,
+                                    height: 50,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Container(
+                                  width: 85,
+                                  height: 85,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[900],
+                                      borderRadius: BorderRadius.circular(15)),
+                                  child: Center(
+                                    child: Image.asset(
+                                      'assets/images/porsche.png',
+                                      width: 50,
+                                      height: 50,
+                                    ),
+                                  )),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Container(
+                                width: 85,
+                                height: 85,
+                                decoration: BoxDecoration(
+                                    color: Colors.grey[900],
+                                    borderRadius: BorderRadius.circular(15)),
+                                child: Center(
+                                  child: Image.asset(
+                                    'assets/images/lamborghini.png',
+                                    width: 50,
+                                    height: 50,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Container(
+                                width: 85,
+                                height: 85,
+                                decoration: BoxDecoration(
+                                    color: Colors.grey[900],
+                                    borderRadius: BorderRadius.circular(15)),
+                                child: Center(
+                                  child: Image.asset(
+                                    'assets/images/benz.png',
+                                    width: 65,
+                                    height: 65,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Container(
+                                  width: 85,
+                                  height: 85,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[900],
+                                      borderRadius: BorderRadius.circular(15)),
+                                  child: Center(
+                                    child: Image.asset(
+                                      'assets/images/tesla.png',
+                                      width: 65,
+                                      height: 65,
+                                    ),
+                                  )),
+                            ],
+                          ),
                         )
                       ],
                     ),
                   ),
-                  Container(
-                    width: 130,
-                    height: 120,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[600],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(CupertinoIcons.car_detailed),
-                        Text('Buy Car')
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 130,
-                    height: 120,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[600],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(CupertinoIcons.car_detailed),
-                        Text('Buy Car')
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: Container(
-                  width: 410,
-                  height: 150,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20)),
                 ),
-              )
-            ],
+                // Container(
+                //   width: 410,
+                //   height: 400,
+                //   color: Colors.amber,
+                // )
+              ],
+            ),
           ),
         ),
       ),
