212a2fe94882aada51ab1484df2d3fff88016d2c
Aaron Djangmah <djangsiness01@gmail.com>
2023-12-18 15:49:40 +0000

    updates

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -14,58 +13,72 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.grey[800],
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: CircleAvatar(
-                    radius: 40,
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: CircleAvatar(
+                      radius: 40,
+                    ),
                   ),
-                ),
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Hello, Aaron',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.yellow,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Welcome Back',
-                      style: TextStyle(fontSize: 15, color: Colors.white),
-                    ),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello, Aaron',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.yellow,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Welcome Back',
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 140),
+                    child: IconButton.outlined(
+                        iconSize: 30,
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.notifications,
+                          color: Colors.white,
+                        )),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: SearchBar(
+                  elevation: MaterialStateProperty.all(3),
+                  backgroundColor: MaterialStatePropertyAll(Colors.grey[800]),
+                  hintText: 'Search',
+                  hintStyle: MaterialStateProperty.all(
+                      const TextStyle(color: Colors.white)),
+                  leading: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  trailing: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.filter_list_outlined,
+                          color: Colors.white,
+                        ))
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 140),
-                  child: IconButton.outlined(
-                      iconSize: 30,
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.notifications,
-                        color: Colors.white,
-                      )),
-                )
-              ],
-            ),
-            SearchBar(
-              backgroundColor: Colors.grey[900],
-              hintText: 'Search',
-              leading: const Icon(Icons.search),
-              trailing: [
-                const VerticalDivider(),
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.filter_list_outlined,
-                    ))
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
