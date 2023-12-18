20b47fae144648ba78ebd1c6b4c6a1eea445dcbb
Aaron Djangmah <djangsiness01@gmail.com>
2023-12-18 12:36:02 +0000

    updates

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,17 +12,17 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[400],
-      appBar: AppBar(
-        backgroundColor: Colors.grey[400],
-        leading: const CircleAvatar(),
-        title: const Column(
-          children: [Text('data'), Text('data')],
-        ),
-        actions: [
-          IconButton.filledTonal(
-              onPressed: () {}, icon: const Icon(Icons.notifications))
+      body: SafeArea(
+          child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(),
+              RichText(text: )
+            ],
+          )
         ],
-      ),
+      )),
     );
   }
 }
