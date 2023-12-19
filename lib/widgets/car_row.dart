a7cc6d4eb48ca84a052a7ce88be42a581d5e9259
Aaron Djangmah <djangsiness01@gmail.com>
2023-12-19 16:05:27 +0000

    updates

--- lib/widgets/car_row.dart
+++ lib/widgets/car_row.dart
@@ -1,6 +1,5 @@
-import 'package:car_rental/widgets/colour_buttons.dart';
+import 'package:car_rental/widgets/popular_car_card.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class CarRow extends StatelessWidget {
   const CarRow({
@@ -9,43 +8,13 @@ class CarRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return const Stack(
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            PopularCarCard(),
-            Container(
-              width: 180,
-              height: 180,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  bottomRight: Radius.circular(20),
-                  bottomLeft: Radius.circular(20),
-                ),
-                color: Colors.grey[700],
-                boxShadow: const [
-                  BoxShadow(
-                      color: Colors.yellow, blurRadius: 1, spreadRadius: 2)
-                ],
-              ),
-              child: const Padding(
-                padding: EdgeInsets.only(top: 40),
-                child: Column(
-                  children: [
-                    ColourButtons(),
-                    Text(
-                      'data',
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+          children: [PopularCarCard(), PopularCarCard()],
         ),
       ],
     );
   }
 }
-
