3c0e2dd209152366706b5b71c023dadb93766fd9
Aaron Djangmah <djangsiness01@gmail.com>
2023-12-20 16:14:17 +0000

    updates

--- lib/stackimpl.dart
+++ lib/stackimpl.dart
@@ -1,13 +1,13 @@
-import 'package:car_rental/widgets/car_row.dart';
-import 'package:flutter/material.dart';
+// import 'package:car_rental/widgets/car_row.dart';
+// import 'package:flutter/material.dart';
 
-class Impl extends StatelessWidget {
-  const Impl({super.key});
+// class Impl extends StatelessWidget {
+//   const Impl({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CarRow(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: CarRow(),
+//     );
+//   }
+// }
